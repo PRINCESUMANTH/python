a693981711860a625763509d98b1db7220ed5cdf
P Sumanth Reddy <52598407+PRINCESUMANTH@users.noreply.github.com>
2021-12-14 00:09:06 +0530

    Update NO_with_stars.py

--- NO_with_stars.py
+++ NO_with_stars.py
@@ -1,3 +1,9 @@
+#this program print the NO letter with the star's using python programming language.....
+
+
+
+
+
 for i in range(10):
     if(i==0 or i==9):
         print("*"," "*13,"*"," "*3,"*"*12)
